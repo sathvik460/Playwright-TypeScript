6db664f8ad7730bccd25b221f593a3f617659ea2
Sathvik <sathvik460@gmail.com>
2025-04-10 11:31:54 +0530

    added validation using multiple locators

---
+++ tests/locators-test.spec.ts
@@ -0,0 +1,28 @@
+import {test, expect} from '@playwright/test'
+
+test('Different types of locators', async({page})=>{
+
+
+  //await page.goto('https://github.com/sathvik460');
+  
+  // getByRole
+ // await page.getByRole('link',{name:'Sign in'}).click();
+
+  // getByLabel
+
+  //await page.getByLabel('Homepage', {exact: true}).first().click();
+
+  // getByAltText
+
+  //await page.getByAltText("View sathvik460's full-sized avatar").click();
+
+  // getByTextId
+
+  // await page.getByTestId("repositories").first().click();
+
+  // getByTitle
+
+  await page.goto('https://www.google.com/');
+  await page.getByTitle('Search').first().fill('playwright');
+
+})
